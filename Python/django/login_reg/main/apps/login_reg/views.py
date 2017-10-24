209f300af4304a2b0c5c1ce5acb62f567c74a772
Erick Smith <smith.s.erick@gmail.com>
2017-10-24 14:52:56 -0700

    folder moved

--- Python/django/login_reg/main/apps/login_reg/views.py
+++ Python/django/login_reg/main/apps/login_reg/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import User
+import bcrypt
 
 # Create your views here.
 def index(request):
     return render(request, 'login_reg/index.html')
 
-def process(request):
+def register(request):
+    if request.method == 'POST': 
+        errors = User.objects.validator(request.POST)
+        if len(errors['error']) or errors.get('password') is not None:
+            for error in errors['error']:
+                messages.error(request, error)
+            if errors.get('password') is not None:
+                for error in errors['password']:
+                    messages.error(request, error)
+                return redirect(index)
+        hashed_password = User.objects.password_hasher(request.POST['password'])
+        User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=hashed_password)
+        return redirect(index)
+
+def signIn(request):
     if request.method == 'POST':
-        pass
-    return redirect(index)
+        user = User.objects.get(email=request.POST['email'])
+        if User.objects.password_checker(request.POST['password'], user.password):
+            request.session['name'] = user.first_name
+            return redirect('/success')
+        else:
+            return redirect(index)
 
 def success(request):
     return render(request, 'login_reg/success.html')
